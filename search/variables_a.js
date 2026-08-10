ffe4f1c32eeb99fbaf047cb5e987cf4889bac2a0
hijimasa <hijimasa@users.noreply.github.com>
2026-08-10 14:27:01 +0000

    deploy: 91698e3593abdb04baac022a670cc52fee027238

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['latitude_0',['latitude',['../classUnitySensors_1_1DataType_1_1Geometry_1_1GeoCoordinate.html#a9a1f35c076f4c135afe7932e37f00773',1,'UnitySensors::DataType::Geometry::GeoCoordinate']]],
-  ['localtoworldmatrix_1',['localToWorldMatrix',['../structUnitySensors_1_1Sensor_1_1LiDAR_1_1IUpdateRaycastCommandsJob.html#acdfe11e1cfdff0c690633eff7ccb66a7',1,'UnitySensors::Sensor::LiDAR::IUpdateRaycastCommandsJob']]],
-  ['longitude_2',['longitude',['../classUnitySensors_1_1DataType_1_1Geometry_1_1GeoCoordinate.html#a9c8f0216bad770e9df63d55969fe894e',1,'UnitySensors::DataType::Geometry::GeoCoordinate']]]
+  ['kb4label_0',['kb4Label',['../classUnitySensors_1_1Editor_1_1FisheyeCameraEditor.html#a842ed677c58e4aa4324cbe46bbd393bc',1,'UnitySensors::Editor::FisheyeCameraEditor']]],
+  ['kb4prop_1',['kb4Prop',['../classUnitySensors_1_1Editor_1_1FisheyeCameraEditor.html#a41ca10ee79f8ef23b7a271bd2b06100d',1,'UnitySensors::Editor::FisheyeCameraEditor']]]
 ];
